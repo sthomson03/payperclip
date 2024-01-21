7be730711eb87599b2a84c99485410d689f4c7f0
Sam Thomson <33004011+sthomson03@users.noreply.github.com>
2024-01-21 14:04:58 +0400

    V0.5
    - Implemented "expand" button functionality.
    - Hover over "expand" and "react" buttons functioning correctly.
    - Created empty "login.html" and "register.html" files.

--- sharedoc.js
+++ sharedoc.js
@@ -6,9 +6,24 @@ function toggleDarkMode(event) {
     var darkModeButton = document.querySelector(".dark-mode-button");
     darkModeButton.classList.toggle("active");
 }
+
+function toggleExpand(event) {
+    event.preventDefault();
+    var container = event.target.closest('.announcements-container');
+    container.classList.toggle("expand");
+
+    var expandButton = container.querySelector(".expand-button");
+    expandButton.classList.toggle("active");
+}
 	
 document.addEventListener("DOMContentLoaded", function() {
     	var darkModeButton = document.querySelector(".dark-mode-button");
     	darkModeButton.addEventListener("click", toggleDarkMode);
     	darkModeButton.addEventListener("touchstart", toggleDarkMode);
+
+        var expandButtons = document.querySelectorAll(".expand-button");
+        expandButtons.forEach(function(expandButton) {
+            expandButton.addEventListener("click", toggleExpand);
+            expandButton.addEventListener("touchstart", toggleExpand);
+        });
 });
